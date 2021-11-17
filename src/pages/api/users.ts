bd291eb58e8447c1e9684cec24046a8242caf050
Joel Maciel <soldado25461@gmail.com>
2021-11-17 13:21:56 -0300

    API routes

---
+++ src/pages/api/users.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request, response) => {
+  const users = [
+    { id: 1, name: "Diego" },
+    { id: 2, name: "Joel" },
+    { id: 3, name: "Rafa" },
+  ];
+  return response.json(users);
+};
