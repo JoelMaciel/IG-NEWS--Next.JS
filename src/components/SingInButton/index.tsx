c24306826181fafd8fab339ea1e96273f49f5cc4
Joel Maciel <soldado25461@gmail.com>
2021-11-18 11:11:15 -0300

    autenticaçao com Next Auth

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,17 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn ,signOut, useSession} from "next-auth/client"
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.singInButton}>
+  const [session] = useSession();
+
+  console.log(session);
+  
+
+  return session ? (
+    <button type="button" 
+    className={styles.singInButton}
+    onClick={()=> signOut()}
+    >
       <FaGithub color="#04d361" />
-      Joel Maciel
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.singInButton}>
+    <button type="button"
+     className={styles.singInButton}
+     onClick={()=> signIn('github')}
+     >
       <FaGithub color="#eba417" />
       Sing in with GitHub
     </button>
