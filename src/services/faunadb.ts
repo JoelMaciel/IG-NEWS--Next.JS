6fc323f93e38718bb42b84a1dc55c08d19a67f44
Joel Maciel <soldado25461@gmail.com>
2021-12-03 19:22:33 -0300

    error 404 Subscribe

--- src/services/faunadb.ts
+++ src/services/faunadb.ts
@@ -1,6 +1,7 @@
 import {Client} from "faunadb";
-console.log(process.env.NEXT_PUBLIC_FAUNA_KEY);
+console.log(process.env.FAUNADB_KEY);
 
 export const fauna = new Client({
-    secret: process.env.NEXT_PUBLIC_FAUNA_KEY
-})
+    secret: process.env.FAUNADB_KEY,
+    domain: "db.us.fauna.com",
+  });
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,12 +2,12 @@ import Stripe from "stripe";
 import {version} from '../../package.json'
 
 export const stripe = new Stripe(
-    process.env.NEXT_PUBLIC_STRIPE_API_KEY,
+    process.env.STRIPE_API_KEY,
     {
         apiVersion:'2020-08-27',
         appInfo: {
             name: 'Ignews',
-            version
+            version,
         },
     }
 );
