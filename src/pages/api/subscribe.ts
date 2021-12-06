d009cda42fc58ad6d6bb2692d7055ff8863d3665
Joel Maciel <soldado25461@gmail.com>
2021-12-06 10:07:47 -0300

    erro 500, You do not have permission to sign in , Sign in.

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,15 +1,43 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { stripe } from "../../../services/stripe";
+import { stripe } from "../../services/stripe";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/faunadb";
+import { query as q } from 'faunadb';
+
+type  User ={
+  ref: {
+    id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
       const session = await  getSession({req}) 
 
+      const user =  await fauna.query<User>(
+        q.Get(
+          q.Match(
+            q.Index('user_by_email'),
+            q.Casefold(session.user.email)
+          )
+        )
+      )
+
     const stripeCustomer = await stripe.customers.create({
         email: session.user.email,
         //metadata
     })
+
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'), user.ref.id),
+        {
+          data: {
+            stripe_customer_id: stripeCustomer.id
+          }
+        }
+      )
+    )
 
       const stripeCheckoutSession = await stripe.checkout.sessions.create({
           customer: stripeCustomer.id,
